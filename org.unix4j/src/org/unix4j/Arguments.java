579895fb2ef497bb650824eaca523f9de1b3f2b4
terz <terzerm@gmail.com>
2012-06-16 02:07:24 +1000

    nice args works now

---
+++ org.unix4j/src/org/unix4j/Arguments.java
@@ -0,0 +1,38 @@
+package org.unix4j;
+
+import java.util.Map;
+
+public interface Arguments {
+	void resolve(Map<String, String> variables);
+	
+	class Void implements Arguments {
+		@Override
+		public void resolve(Map<String, String> variables) {
+			//nothing to do
+		}
+	}
+	class Variables {
+		public static String encode(String name) {
+			return "{" + name + "}";
+		}
+		public static String encode(String name, int index) {
+			if (index < 0) {
+				throw new IndexOutOfBoundsException("index must be >= 0, but was " + index);
+			}
+			return "{" + name + "[" + index + "]}";
+		}
+		public static boolean isVariable(String expression) {
+			final int len = expression == null ? 0 : expression.length();
+			return len > 2 && expression.charAt(0) == '{' && expression.charAt(len - 1) == '}'; 
+		}
+		public static String resolve(String expression, Map<String,String> variables) {
+			if (isVariable(expression)) {
+				if (variables.containsKey(expression)) {
+					return variables.get(expression);
+				}
+				throw new IllegalArgumentException("unresolved variable: " + expression);
+			}
+			return expression;
+		}
+	}
+}
---
+++ org.unix4j/src/org/unix4j/JoinedOutput.java
@@ -0,0 +1,47 @@
+package org.unix4j;
+
+import java.util.LinkedList;
+
+import org.unix4j.io.BufferedInput;
+
+/**
+ * 
+ */
+public class JoinedOutput implements Output {
+
+	private final Command<?> joinedCommand;
+	private final LinkedList<String> buffer = new LinkedList<String>();
+	private final BufferedInput input = new BufferedInput(buffer);
+	private final Output output;
+
+	/**
+	 * Constructor with joined command and output for second command.
+	 * 
+	 * @param joinedCommand
+	 *            the joined command
+	 * @param output
+	 *            the output for the second command
+	 */
+	public JoinedOutput(Command<?> joinedCommand, Output output) {
+		this.joinedCommand = joinedCommand;
+		this.output = output;
+	}
+
+	// --- output ---\\
+
+	@Override
+	public void writeLine(String line) {
+		buffer.add(line);
+		if (joinedCommand.getType().isLineByLine()) {
+			joinedCommand.execute(input, output);
+		}
+	}
+
+	@Override
+	public void finish() {
+		if (!joinedCommand.getType().isLineByLine()) {
+			joinedCommand.execute(input, output);
+		}
+		output.finish();
+	}
+}
