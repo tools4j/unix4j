04fad779d6afd9156bb15e2c1409e63a56b05039
terz <terzerm@gmail.com>
2012-06-21 00:31:31 +1000

    merge cut/grep into new builder/command structure

---
+++ org.unix4j/tst/org/unix4j/builder/GenericCommandBuilderTest.java
@@ -0,0 +1,97 @@
+package org.unix4j.builder;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.unix4j.builder.DefaultCommandBuilder;
+import org.unix4j.builder.GenericCommandBuilder;
+import org.unix4j.builder.Unix4jCommandBuilder;
+import org.unix4j.command.unix.Cut;
+import org.unix4j.command.unix.Echo;
+import org.unix4j.command.unix.Grep;
+import org.unix4j.command.unix.Ls;
+import org.unix4j.command.unix.Sort;
+import org.unix4j.command.unix.Xargs;
+import org.unix4j.io.FileInput;
+import org.unix4j.io.Input;
+import org.unix4j.io.Output;
+import org.unix4j.io.StreamOutput;
+
+public class GenericCommandBuilderTest {
+	
+	private Unix4jCommandBuilder unix4j;
+	private Output output;
+
+	@SuppressWarnings("unchecked")
+	private static Unix4jCommandBuilder createCommandBuilder(Input input) {
+		final DefaultCommandBuilder defaultCommandBuilder = input == null ? new DefaultCommandBuilder() : new DefaultCommandBuilder(input);
+		return GenericCommandBuilder.createCommandBuilder(Unix4jCommandBuilder.class, defaultCommandBuilder, Ls.FACTORY, Grep.FACTORY, Echo.FACTORY, Sort.FACTORY, Cut.FACTORY, Xargs.FACTORY);
+	}
+	@Before
+	public void beforeEach() {
+		unix4j = createCommandBuilder(null);
+		output = null;
+	}
+
+	@After
+	public void afterEach() {
+		System.out.println(">>> " + unix4j);
+		if (output == null) {
+			unix4j.execute();
+		} else {
+			unix4j.execute(output);
+		}
+	}
+	
+	@Test
+	public void testLs() {
+		unix4j.ls();
+	}
+	@Test
+	public void testLsFile() {
+		unix4j.ls(new File("src"));
+	}
+	@Test
+	public void testLsSort() {
+		unix4j.ls().sort();
+	}
+	@Test
+	public void testLsSortDesc() {
+		unix4j.ls().sort(Sort.Option.descending);
+	}
+	@Test
+	public void testEcho() {
+		unix4j.echo("Hello world");
+	}
+	@Test
+	public void testEcho2() {
+		unix4j.echo("Hello", "world");
+	}
+	@Test
+	public void testEchoGrepMatch() {
+		unix4j.echo("Hello world").grep("world");
+	}
+	@Test
+	public void testEchoGrepNoMatch() {
+		unix4j.echo("Hello WORLD").grep("world");
+	}
+	@Test
+	public void testEchoGrepMatchIgnoreCase() {
+		unix4j.echo("Hello WORLD").grep("world", Grep.Option.ignoreCase);
+	}
+	@Test
+	public void testLsXargsEcho() {
+		unix4j.ls().xargs().echo("XARGS OUTPUT:", Xargs.XARG);
+	}
+	@Test
+	public void testFromFile() {
+		unix4j = createCommandBuilder(new FileInput(new File(".classpath"))).sort();
+	}
+	@Test
+	public void testToSystemError() {
+		output = new StreamOutput(System.err);
+		unix4j.echo("Hello ERROR").grep("error", Grep.Option.ignoreCase);
+	}
+}
