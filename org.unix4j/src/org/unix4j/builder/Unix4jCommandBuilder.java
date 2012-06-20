bf24003dc9fc061e777a5ce96340b1a39eb085ef
terz <terzerm@gmail.com>
2012-06-21 00:32:08 +1000

    merge cut/grep into new builder/command structure

--- org.unix4j/src/org/unix4j/builder/Unix4jCommandBuilder.java
+++ org.unix4j/src/org/unix4j/builder/Unix4jCommandBuilder.java
@@ -1,10 +1,11 @@
 package org.unix4j.builder;
 
+import org.unix4j.command.unix.Cut;
 import org.unix4j.command.unix.Echo;
 import org.unix4j.command.unix.Grep;
 import org.unix4j.command.unix.Ls;
 import org.unix4j.command.unix.Sort;
 import org.unix4j.command.unix.Xargs;
 
-public interface Unix4jCommandBuilder extends CommandBuilder, Ls.Interface<Unix4jCommandBuilder>, Grep.Interface<Unix4jCommandBuilder>, Echo.Interface<Unix4jCommandBuilder>, Sort.Interface<Unix4jCommandBuilder>, Xargs.Interface<Unix4jCommandBuilder> {
+public interface Unix4jCommandBuilder extends CommandBuilder, Ls.Interface<Unix4jCommandBuilder>, Grep.Interface<Unix4jCommandBuilder>, Echo.Interface<Unix4jCommandBuilder>, Sort.Interface<Unix4jCommandBuilder>, Cut.Interface<Unix4jCommandBuilder>, Xargs.Interface<Unix4jCommandBuilder> {
 }
