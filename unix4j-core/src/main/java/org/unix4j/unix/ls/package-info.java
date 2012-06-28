6cc73c7522c894c5d211ef019fdf2edc8e105aee
terz <terzerm@gmail.com>
2012-06-28 23:45:53 +1000

    make package visible classes appear in javadoc

---
+++ unix4j-core/src/main/java/org/unix4j/unix/ls/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Contains private classes used by the {@link org.unix4j.unix.Ls ls} command. 
+ * Most of the classes have package visibility and do not need to be accessed by 
+ * application code directly. 
+ */
+package org.unix4j.unix.ls;
