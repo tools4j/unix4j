059ddf65b4c32fbb22228d983f1b4724dddb8d4f
terz <terzerm@gmail.com>
2012-06-28 23:27:51 +1000

    make perf project work again

---
+++ unix4j-perf/src/test/java/org/unix4j/unix/GrepPerfTest.java
@@ -0,0 +1,22 @@
+package org.unix4j.unix;
+
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import org.junit.Test;
+import org.unix4j.Unix4j;
+import org.unix4j.io.WriterOutput;
+
+public class GrepPerfTest {
+	@Test
+	public void testGrep_simple1() {
+		final String regex = "This";
+		runGrep( regex );
+	}
+
+	private void runGrep(final String expression, final Grep.Option... options){
+		final InputStream testFileAsInputStream = this.getClass().getClassLoader().getResourceAsStream("1.1M_test_file.txt");
+		final StringWriter actualOutputStringWriter = new StringWriter();
+		Unix4j.builder(testFileAsInputStream).grep(expression, options).execute(new WriterOutput(actualOutputStringWriter));
+	}
+}
