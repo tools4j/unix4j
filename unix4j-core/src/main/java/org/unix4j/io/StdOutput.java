f0d4a9a9ab49fe837d84cc31b7387637b7e5b54f
terz <terzerm@gmail.com>
2012-07-12 22:56:19 +1000

    new module structure for codegen

--- unix4j-core/src/main/java/org/unix4j/io/StdOutput.java
+++
@@ -1,7 +0,0 @@
-package org.unix4j.io;
-
-public class StdOutput extends StreamOutput {
-	public StdOutput() {
-		super(System.out);
-	}
-}
--- unix4j-core/src/main/java/org/unix4j/util/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Provides a set of utility classes used throughout the unix4j framework. 
- */
-package org.unix4j.util;
