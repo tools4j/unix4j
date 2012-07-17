06cdb09fa7efc4dc1d8b255541873b9e3e4e42c5
Ben <bjwarner@gmail.com>
2012-07-17 23:38:20 +1000

    Addition of broken CommandChainingTest to show chaining problems.

---
+++ unix4j-core/unix4j-command-impl/src/test/java/org/unix4j/unix/CommandChainingTest.java
@@ -0,0 +1,40 @@
+package org.unix4j.unix;
+
+import org.junit.Test;
+import org.unix4j.Unix4j;
+import org.unix4j.util.MultilineString;
+
+import static org.junit.Assert.assertEquals;
+
+public class CommandChainingTest {
+	private final static MultilineString input;
+	static {
+		input = new MultilineString();
+		input
+				.appendLine("This is a test blah blah blah")
+				.appendLine("This is a test blah blah")
+				.appendLine("This is a test one two three")
+				.appendLine("one")
+				.appendLine("a")
+				.appendLine("")
+				.appendLine("two")
+				.appendLine("def\\d123");
+	}
+
+	@Test
+	public void test1() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput
+				.appendLine("This is a test hasblah blah blah")
+				.appendLine("This is a test hasblah blah")
+				.appendLine("This is a test one two three")
+				.appendLine("one")
+				.appendLine("a")
+				.appendLine("")
+				.appendLine("two")
+				.appendLine("def\\d123");
+
+		assertEquals("one", Unix4j.fromString(input.toString()).head(4).tail(1).executeToString(false));
+	}
+
+}
