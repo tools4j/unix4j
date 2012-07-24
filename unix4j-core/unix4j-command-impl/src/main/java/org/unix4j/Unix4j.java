492e8976e453f7b05b6cfe765c0c57d00db97f8d
Ben <bjwarner@gmail.com>
2012-07-24 23:25:09 +1000

    Addition of "from file" tests.

--- unix4j-core/unix4j-command-impl/src/main/java/org/unix4j/Unix4j.java
+++ unix4j-core/unix4j-command-impl/src/main/java/org/unix4j/Unix4j.java
@@ -5,9 +5,11 @@ import org.unix4j.builder.Unix4jCommandBuilderImpl;
 import org.unix4j.io.FileInput;
 import org.unix4j.io.Input;
 import org.unix4j.io.StreamInput;
+import org.unix4j.util.Assert;
 
 import java.io.File;
 import java.io.InputStream;
+import java.net.URL;
 
 /**
  * Utility class with static methods serving as starting point to create a
@@ -29,11 +31,37 @@ public class Unix4j {
 
 	/**
 	 * Returns a builder to create a command or command chain reading the input
+	 * from the specified file on the classpath.
+	 *
+	 * @param filePathAndName
+	 *            a path to the file to to redirect to the first command
+	 *            The will need to be on the classpath.  If the file is in the
+	 *            root directory, the filename should be prefixed with a forward
+	 *            slash.  e.g.:
+	 *            <pre>/test-file.txt</pre>
+	 *            If the file is in a package, then the package should be specified
+	 *            prefixed with a forward slash, and with each dot "." replaced
+	 *            with a forward slash.  e.g.:
+	 *            <pre>/org/company/my/package/test-file.txt</pre>
+	 * @return the builder to create the command or command chain
+	 */
+	public static Unix4jCommandBuilder fromFileOnClasspath(String filePathAndName) {
+		Assert.assertArgNotNull("filePathAndName must not be null", filePathAndName);
+		URL url = filePathAndName.getClass().getResource(filePathAndName);
+		if(url==null){
+			throw new IllegalArgumentException("filePathAndName:" + filePathAndName + " references a file which does not exist.");
+		}
+		File file = new File(url.getFile());
+		return fromFile(file);
+	}
+
+	/**
+	 * Returns a builder to create a command or command chain reading the input
 	 * from the specified input stream.
 	 *
 	 * @param in
 	 *            the input stream redirected to the input of the first command
-	 * @return the fromFile to create the command or command chain
+	 * @return the builder to create the command or command chain
 	 */
 	public static Unix4jCommandBuilder fromInputStream(InputStream in) {
 		return fromInput(new StreamInput(in));
@@ -45,7 +73,7 @@ public class Unix4j {
 	 *
 	 * @param in
 	 *            the input passed to the first command
-	 * @return the fromFile to create the command or command chain
+	 * @return the builder to create the command or command chain
 	 */
 	public static Unix4jCommandBuilder fromInput(Input in) {
 		return new Unix4jCommandBuilderImpl(in);
@@ -57,7 +85,7 @@ public class Unix4j {
 	 *
 	 * @param input
 	 *            the String written to the input of the first command
-	 * @return the command to create the command or command chain
+	 * @return the builder to create the command or command chain
 	 */
 	public static Unix4jCommandBuilder fromString(String input) {
 		return (new Unix4jCommandBuilderImpl()).echo(input);
