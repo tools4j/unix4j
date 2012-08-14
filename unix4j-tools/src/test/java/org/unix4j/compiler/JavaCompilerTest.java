e0b0277983193cffc1b8d9807a769a900631cba8
Ben <bjwarner@gmail.com>
2012-08-14 22:17:35 +1000

    Updates to JavaCompilerTest to use non-workspace file locations.

--- unix4j-tools/src/test/java/org/unix4j/compiler/JavaCompilerTest.java
+++ unix4j-tools/src/test/java/org/unix4j/compiler/JavaCompilerTest.java
@@ -1,18 +1,44 @@
 package org.unix4j.compiler;
 
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
-
-import org.junit.Test;
+import java.net.URL;
 
 public class JavaCompilerTest {
 
 	@Test
 	public void testCompile() throws ClassNotFoundException, IOException {
-		final File outputFolder = new File("target/test-" + getClass().getSimpleName());
-		if (!outputFolder.exists()) {
-			outputFolder.mkdirs();
+		final File testOutputDir = getOutputDir();
+		final File testFilesDir = getInputDir();
+
+		System.out.println("Running compiler in directory: " + testFilesDir + " to output directory:" + testOutputDir);
+		new JavaCompiler().compile(testOutputDir, testFilesDir);
+	}
+
+	/**
+	 * @return A directory to write files to. If using maven structure, should exist at:
+	 * target/test-classes/org/unix4j/compiler/testfiles
+	 */
+	private File getInputDir() {
+		final String thisClassPackageAndName = "/" + this.getClass().getName().replace(".","/") + ".class";
+		final URL thisClassURL = this.getClass().getResource(thisClassPackageAndName);
+		final File thisClassFile = new File(thisClassURL.getFile());
+		return new File(thisClassFile.getParent() + "/testfiles");
+	}
+
+	/**
+	 * @return A directory to write files to. If using maven structure, should exist at:
+	 * target/test-classes/test-JavaCompilerTest
+	 */
+	private File getOutputDir() {
+		final URL outputDirURL = this.getClass().getResource("/");
+		final File outputDirAsFile = new File(outputDirURL.getFile());
+		final File testOutputDir = new File(outputDirAsFile.getPath() + "/test-" + this.getClass().getSimpleName());
+		if (!testOutputDir.exists()) {
+			testOutputDir.mkdirs();
 		}
-		new JavaCompiler().compile(outputFolder, new File("src/test/java/org/unix4j/compiler/testfiles"));
+		return testOutputDir;
 	}
 }
