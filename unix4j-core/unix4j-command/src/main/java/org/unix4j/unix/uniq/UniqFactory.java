2948525c0bf9b5c8ae5fb9e71c9b99a91b6e3b51
terz <terzerm@gmail.com>
2012-09-01 14:21:37 +1000

    uniq command impl with unit test

--- unix4j-core/unix4j-command/src/main/java/org/unix4j/unix/uniq/UniqFactory.java
+++ unix4j-core/unix4j-command/src/main/java/org/unix4j/unix/uniq/UniqFactory.java
@@ -1,17 +1,21 @@
 package org.unix4j.unix.uniq;
 
+import java.io.File;
+
+import org.unix4j.unix.Uniq.Interface;
+import org.unix4j.unix.Uniq.Options;
 
 /**
- * Factory class returning a new TODO {@code UniqCommand} instance from every signature
- * method.
+ * Factory class returning a new {@link UniqCommand} instance from every
+ * signature method.
  */
-public final class UniqFactory /*implements Interface<UniqCommand> */ {
-	
+public final class UniqFactory implements Interface<UniqCommand> {
+
 	/**
 	 * The singleton instance of this factory.
 	 */
 	public static final UniqFactory INSTANCE = new UniqFactory();
-	
+
 	/**
 	 * Private, only used to create singleton instance.
 	 */
@@ -19,4 +23,41 @@ public final class UniqFactory /*implements Interface<UniqCommand> */ {
 		super();
 	}
 
+	@Override
+	public UniqCommand uniq() {
+		return new UniqCommand(new UniqArguments());
+	}
+
+	@Override
+	public UniqCommand uniq(File file) {
+		final UniqArguments args = new UniqArguments();
+		args.setFile(file);
+		return new UniqCommand(args);
+	}
+
+	@Override
+	public UniqCommand uniq(String path) {
+		final UniqArguments args = new UniqArguments();
+		args.setPath(path);
+		return new UniqCommand(args);
+	}
+
+	@Override
+	public UniqCommand uniq(Options options) {
+		return new UniqCommand(new UniqArguments(options));
+	}
+
+	@Override
+	public UniqCommand uniq(Options options, File file) {
+		final UniqArguments args = new UniqArguments(options);
+		args.setFile(file);
+		return new UniqCommand(args);
+	}
+
+	@Override
+	public UniqCommand uniq(Options options, String path) {
+		final UniqArguments args = new UniqArguments(options);
+		args.setPath(path);
+		return new UniqCommand(args);
+	}
 }
---
+++ unix4j-core/unix4j-command/src/test/java/org/unix4j/unix/UniqTest.java
@@ -0,0 +1,167 @@
+package org.unix4j.unix;
+
+import java.io.StringWriter;
+
+import org.junit.Test;
+import org.unix4j.Unix4j;
+import org.unix4j.unix.uniq.UniqOptions;
+import org.unix4j.util.MultilineString;
+
+public class UniqTest {
+	private final static String LINE1 = "This is a test blah blah blah";
+	private final static String LINE2 = "This is a test blah blah de blah";
+	private final static String LINE3 = "This is a test one two three";
+	private final static String LINE4 = "one";
+	private final static String LINE5 = "one";
+	private final static String LINE6 = "";
+	private final static String LINE7 = "two";
+	private final static String LINE8 = "two";
+	private final static String LINE9 = "two";
+	private final static String LINE10 = "three";
+	private final static String LINE11 = "three";
+	private final static String LINE12 = "one";
+	private final static String LINE13 = "two";
+	private final static String LINE14 = "three";
+	private final static String LINE15 = "This is a test blah blah blah";
+	private final static String LINE16 = "Last line is different, has no line ending";
+	private final static MultilineString input;
+
+	static {
+		input = new MultilineString();
+		input.appendLine(LINE1)
+				.appendLine(LINE2)
+				.appendLine(LINE3)
+				.appendLine(LINE4)
+				.appendLine(LINE5)
+				.appendLine(LINE6)
+				.appendLine(LINE7)
+				.appendLine(LINE8)
+				.appendLine(LINE9)
+				.appendLine(LINE10)
+				.appendLine(LINE11)
+				.appendLine(LINE12)
+				.appendLine(LINE13)
+				.appendLine(LINE14)
+				.appendLine(LINE15)
+				.appendLine(LINE16);
+	}
+
+	@Test
+	public void testUniq() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(LINE1);
+		expectedOutput.appendLine(LINE2);
+		expectedOutput.appendLine(LINE3);
+		expectedOutput.appendLine(LINE4);
+		expectedOutput.appendLine(LINE6);
+		expectedOutput.appendLine(LINE7);
+		expectedOutput.appendLine(LINE10);
+		expectedOutput.appendLine(LINE12);
+		expectedOutput.appendLine(LINE13);
+		expectedOutput.appendLine(LINE14);
+		expectedOutput.appendLine(LINE15);
+		expectedOutput.appendLine(LINE16);
+		assertUniq(input, UniqOptions.NO_OPTIONS, expectedOutput);
+	}
+
+	@Test
+	public void testUniq_uniqueOnly() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(LINE1);
+		expectedOutput.appendLine(LINE2);
+		expectedOutput.appendLine(LINE3);
+		expectedOutput.appendLine(LINE6);
+		expectedOutput.appendLine(LINE12);
+		expectedOutput.appendLine(LINE13);
+		expectedOutput.appendLine(LINE14);
+		expectedOutput.appendLine(LINE15);
+		expectedOutput.appendLine(LINE16);
+		assertUniq(input, Uniq.OPTIONS.uniqueOnly, expectedOutput);
+	}
+
+	@Test
+	public void testUniq_duplicatesOnly() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(LINE4);
+		expectedOutput.appendLine(LINE7);
+		expectedOutput.appendLine(LINE10);
+		assertUniq(input, Uniq.OPTIONS.duplicatedOnly, expectedOutput);
+	}
+
+	@Test
+	public void testUniq_count() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(makeCountLine(1, LINE1));
+		expectedOutput.appendLine(makeCountLine(1, LINE2));
+		expectedOutput.appendLine(makeCountLine(1, LINE3));
+		expectedOutput.appendLine(makeCountLine(2, LINE4));
+		expectedOutput.appendLine(makeCountLine(1, LINE6));
+		expectedOutput.appendLine(makeCountLine(3, LINE7));
+		expectedOutput.appendLine(makeCountLine(2, LINE10));
+		expectedOutput.appendLine(makeCountLine(1, LINE12));
+		expectedOutput.appendLine(makeCountLine(1, LINE13));
+		expectedOutput.appendLine(makeCountLine(1, LINE14));
+		expectedOutput.appendLine(makeCountLine(1, LINE15));
+		expectedOutput.appendLine(makeCountLine(1, LINE16));
+		assertUniq(input, Uniq.OPTIONS.count, expectedOutput);
+	}
+
+	@Test
+	public void testUniq_Global() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(LINE1);
+		expectedOutput.appendLine(LINE2);
+		expectedOutput.appendLine(LINE3);
+		expectedOutput.appendLine(LINE4);
+		expectedOutput.appendLine(LINE6);
+		expectedOutput.appendLine(LINE7);
+		expectedOutput.appendLine(LINE10);
+		expectedOutput.appendLine(LINE16);
+		assertUniq(input, Uniq.OPTIONS.global, expectedOutput);
+	}
+
+	@Test
+	public void testUniq_uniqueOnlyGlobal() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(LINE2);
+		expectedOutput.appendLine(LINE3);
+		expectedOutput.appendLine(LINE6);
+		expectedOutput.appendLine(LINE16);
+		assertUniq(input, Uniq.OPTIONS.uniqueOnly.global, expectedOutput);
+	}
+
+	@Test
+	public void testUniq_duplicatesOnlyGlobal() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(LINE1);
+		expectedOutput.appendLine(LINE4);
+		expectedOutput.appendLine(LINE7);
+		expectedOutput.appendLine(LINE10);
+		assertUniq(input, Uniq.OPTIONS.duplicatedOnly.g, expectedOutput);
+	}
+
+	@Test
+	public void testUniq_countGlobal() {
+		final MultilineString expectedOutput = new MultilineString();
+		expectedOutput.appendLine(makeCountLine(2, LINE1));
+		expectedOutput.appendLine(makeCountLine(1, LINE2));
+		expectedOutput.appendLine(makeCountLine(1, LINE3));
+		expectedOutput.appendLine(makeCountLine(3, LINE4));
+		expectedOutput.appendLine(makeCountLine(1, LINE6));
+		expectedOutput.appendLine(makeCountLine(4, LINE7));
+		expectedOutput.appendLine(makeCountLine(3, LINE10));
+		expectedOutput.appendLine(makeCountLine(1, LINE16));
+		assertUniq(input, Uniq.OPTIONS.count.global, expectedOutput);
+	}
+
+	private String makeCountLine(int count, String line) {
+		return "   " + count + " " + line;
+	}
+
+	private void assertUniq(final MultilineString input, Uniq.Options options, final MultilineString expectedOutput){
+		final StringWriter actualOutputStringWriter = new StringWriter();
+		Unix4j.from(input.toInput()).uniq(options).toWriter(actualOutputStringWriter);
+		final MultilineString actualOutput = new MultilineString(actualOutputStringWriter.toString());
+		actualOutput.assertMultilineStringEquals(expectedOutput);
+	}
+}
