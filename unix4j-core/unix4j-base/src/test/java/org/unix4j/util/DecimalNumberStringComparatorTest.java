3a226483c403c0136dcb003134a628e3ba08ed87
terz <terzerm@gmail.com>
2012-09-24 00:10:56 +1000

    sort and generic sort

--- unix4j-core/unix4j-base/src/test/java/org/unix4j/util/DecimalNumberStringComparatorTest.java
+++ unix4j-core/unix4j-base/src/test/java/org/unix4j/util/DecimalNumberStringComparatorTest.java
@@ -5,7 +5,6 @@ import java.math.BigInteger;
 import java.util.Random;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -67,6 +66,83 @@ public class DecimalNumberStringComparatorTest {
 	}
 	
 	@Test
+	public void testSmallDecimals() {
+		assertSmaller("1.1", "1.2");
+		assertSmaller("2.2", "2.3");
+		assertSmaller("3.3", "3.4");
+		assertSmaller("4.4", "4.5");
+		assertSmaller("5.5", "5.6");
+		assertSmaller("6.6", "6.7");
+		assertSmaller("7.7", "7.8");
+		assertSmaller("8.8", "8.9");
+		assertGreater("9.9", "9.10");
+		assertEqual("10.1", "10.10");
+		
+		assertGreater("-1.1", "-1.2");
+		assertGreater("-2.2", "-2.3");
+		assertGreater("-3.3", "-3.4");
+		assertGreater("-4.4", "-4.5");
+		assertGreater("-5.5", "-5.6");
+		assertGreater("-6.6", "-6.7");
+		assertGreater("-7.7", "-7.8");
+		assertGreater("-8.8", "-8.9");
+		assertSmaller("-9.9", "-9.10");
+		assertEqual("-10.1", "-10.10");
+		
+		assertGreater("1.1", "-1.1");
+		assertGreater("1.2", "-1.2");
+		assertGreater("1.3", "-1.3");
+		assertGreater("1.4", "-1.4");
+		assertGreater("1.5", "-1.5");
+		assertGreater("1.6", "-1.6");
+		assertGreater("1.7", "-1.7");
+		assertGreater("1.8", "-1.8");
+		assertGreater("1.9", "-1.9");
+		assertGreater("1.10", "-1.10");
+
+		assertSmaller("1.00", "10.0");
+		assertSmaller("10.000", "100.0");
+		assertSmaller("99.999", "999.9");
+		assertGreater("-1.0", "-1.01");
+		assertGreater("0.1", "0.001");
+		assertSmaller("-0.01", "-0.001");
+		assertGreater("-.66", "-.666");
+		
+		assertSmaller(" .1", " \t .2");
+		assertSmaller("-.1", ".0");
+		assertSmaller("-.1", ".1");
+		assertSmaller("-.1", ".2");
+		assertSmaller("-.1", ".10");
+		assertSmaller("-.1", ".2");
+		assertSmaller("-.01", ".01");
+
+		assertSmaller("76", "76.1");
+		assertSmaller("-76", "76.1");
+		assertSmaller("-22", "22.3");
+		assertSmaller("22", "76.34");
+		assertSmaller("76.34", "99");
+		assertSmaller("76.34", "99.");
+		assertSmaller("76.34", "99.0");
+		assertGreater("76.34", "76");
+		assertGreater("76.34", "76.");
+		assertGreater("76.34", "76.0");
+		assertGreater("76.34", "75");
+		assertGreater("76.34", "75.");
+		assertGreater("76.34", "75.0");
+		assertSmaller("76.34", "750");
+		assertSmaller("76.34", "750.0");
+		assertSmaller("76.34", "751");
+		assertGreater("76.34", "75.5");
+
+		assertEqual("76.000", "76");
+		assertEqual("76.000", "76.");
+		assertEqual("76.000", "76.0");
+		assertGreater("76.001", "76");
+		assertGreater("76.001", "76.");
+		assertGreater("76.001", "76.000");
+	}
+
+	@Test
 	public void testAllBytePairs() {
 		final int min = Byte.MIN_VALUE;
 		final int max = Byte.MAX_VALUE;
@@ -106,26 +182,30 @@ public class DecimalNumberStringComparatorTest {
 		}
 	}
 	@Test
-	public void testLargeSpecialIntegers() {
+	public void testRandomVeryLongDecimalPairs() {
+		final int runs = 1000;
+		final int maxPreDecimalDigits = 1000;
+		final int maxPostDecimalDigits = 1000;
+		final StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < runs; i++) {
+			final String val1 = randomDecimal(sb, maxPreDecimalDigits, maxPostDecimalDigits);
+			final String val2 = randomDecimal(sb, maxPreDecimalDigits, maxPostDecimalDigits);
+			final int expected = new BigDecimal(val1).compareTo(new BigDecimal(val2));
+			assertCompare(val1, val2, expected);
+		}
+	}
+	@Test
+	public void testLargeIntegers() {
 		assertSmaller("873948723948192384791283491283749812374981273948170984320548739845", "873948723948192384791283491283749812374981273948170984320548739846");
 		assertSmaller("-873948723948192384791283491283749812374981273948170984320548739845", "-873948723948192384791283491283749812374981273948170984320548739844");
 	}
-	@Ignore	//FIXME make decimals work and enable unit test
-	@Test
-	public void testRandomDoublePairs() {
-		final int runs = 10000;
-		for (int i = 0; i < runs; i++) {
-			final double val1 = rnd.nextDouble();
-			final double val2 = rnd.nextDouble();
-			final int expected = Double.compare(val1, val2);
-			assertCompare(String.valueOf(val1), String.valueOf(val2), expected);
-//			comparator.compare(String.valueOf(val1), String.valueOf(val2));
-//			Long.compare(Long.parseLong(String.valueOf(val1)), Long.parseLong(String.valueOf(val2)));
-//			new BigDecimal(String.valueOf(val1)).compareTo(new BigDecimal(String.valueOf(val2)));
-		}
-	}
 	
-	@Ignore	//FIXME make decimals work and enable unit test
+	@Test
+	public void testLargeDecimals() {
+		assertSmaller("00049502948350938450983540698094560934506983405968.873948723948192384791283491283749812374981273948170984320548739845", "049502948350938450983540698094560934506983405968.873948723948192384791283491283749812374981273948170984320548739846");
+		assertSmaller("-0049502948350938450983540698094560934506983405968.873948723948192384791283491283749812374981273948170984320548739845", "-049502948350938450983540698094560934506983405968.873948723948192384791283491283749812374981273948170984320548739844");
+	}
+
 	@Test
 	public void testRandomDoublePairsAsDecimals() {
 		final int runs = 10000;
@@ -198,6 +278,29 @@ public class DecimalNumberStringComparatorTest {
 		if (rnd.nextBoolean()) sb.append('-');
 		final int digits = 1 + rnd.nextInt(maxDigits);
 		for (int j = 0; j < digits; j++) {
+			sb.append(rnd.nextInt(10));
+		}
+		return sb.toString();
+	}
+	private String randomDecimal(StringBuilder sb, int maxPreDecimalDigits, int maxPostDecimalDigits) {
+		//lets half of the decimals share the integer part with the previous value stored in sb
+		final int preDigits;
+		final int postDigits = rnd.nextInt(1 + maxPostDecimalDigits);
+		if (rnd.nextBoolean()) {
+			preDigits = sb.indexOf(".");
+			sb.setLength(preDigits + 1);
+		} else {
+			sb.setLength(0);
+			preDigits = rnd.nextInt(1 + maxPreDecimalDigits);
+			if (rnd.nextBoolean()) sb.append('-');
+			for (int j = 0; j < preDigits; j++) {
+				sb.append(rnd.nextInt(10));
+			}
+			if (preDigits == 0 || postDigits > 0) {
+				sb.append('.');
+			}
+		}
+		for (int j = 0; j < postDigits; j++) {
 			sb.append(rnd.nextInt(10));
 		}
 		return sb.toString();
