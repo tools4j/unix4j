f6a13a684c56637f899e0fb7b1769b359dde7ec8
terz <terzerm@gmail.com>
2012-09-25 00:17:47 +1000

    javadoc fixes for sort and options

--- unix4j-core/unix4j-command/src/main/resources/codegen/templates/Command.java
+++ unix4j-core/unix4j-command/src/main/resources/codegen/templates/Command.java
@@ -62,7 +62,7 @@ import ${def.pkg.name}.${varName};
  * <p>
  * <table>
 <#foreach opt in def.options?values>
- * <tr><td width="10px"></td><td nowrap="nowrap">{@code -${opt.acronym}}</td><td>&nbsp;&nbsp;</td><td nowrap="nowrap">{@code --${opt.name}}</td><td>&nbsp;</td><td>${opt.desc}</td></tr>
+ * <tr valign="top"><td width="10px"></td><td nowrap="nowrap">{@code -${opt.acronym}}</td><td>&nbsp;&nbsp;</td><td nowrap="nowrap">{@code --${opt.name}}</td><td>&nbsp;</td><td>${opt.desc}</td></tr>
 </#foreach>
  * </table>
 <#else>
