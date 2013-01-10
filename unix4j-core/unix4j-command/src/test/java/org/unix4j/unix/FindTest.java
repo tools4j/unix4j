872c5df49bed93d10dcff0deb156223dd04ff002
ben <bjwarner@gmail.com>
2013-01-10 22:33:15 +1100

    Updates to FindCommand and test files

--- unix4j-core/unix4j-command/src/test/java/org/unix4j/unix/FindTest.java
+++ unix4j-core/unix4j-command/src/test/java/org/unix4j/unix/FindTest.java
@@ -2,11 +2,31 @@ package org.unix4j.unix;
 
 import org.junit.Test;
 import org.unix4j.Unix4j;
+import org.unix4j.context.DefaultExecutionContext;
+import org.unix4j.context.ExecutionContext;
+import org.unix4j.context.ExecutionContextFactory;
+
+import java.io.File;
 
 public class FindTest {
+    private static final class Config implements ExecutionContextFactory {
+        private final FileTest tester;
+        public Config(FileTest tester) {
+            this.tester = tester;
+        }
+        @Override
+        public ExecutionContext createExecutionContext() {
+            final DefaultExecutionContext context = new DefaultExecutionContext();
+            context.setCurrentDirectory(tester.getInputFile());
+            return context;
+        }
+    };
 
-	@Test
-	public void testFind() {
-		Unix4j.find(".", "/home/ben/dev/*/groovy/*/*", "target/*", "asdf").toStdOut();
-	}
+
+    @Test
+    public void testFind() {
+        final FileTest tester = new FileTest(this.getClass());
+        final Config config = new Config(tester);
+        Unix4j.use(config).find(".", "/home/ben/dev/*/groovy/*/*", "target/*", "asdf").toStdOut();
+    }
 }
