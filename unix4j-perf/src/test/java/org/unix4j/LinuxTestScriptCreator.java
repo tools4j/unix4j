d752b8590f7bb61a84e84ca8193ea304a5aabc38
ben <bjwarner@gmail.com>
2013-01-28 23:14:16 +1100

    Fixes to the perf test script creator so that it runs on mac-osx as well as linux.

--- unix4j-perf/src/test/java/org/unix4j/LinuxTestScriptCreator.java
+++ unix4j-perf/src/test/java/org/unix4j/LinuxTestScriptCreator.java
@@ -24,9 +24,15 @@ public class LinuxTestScriptCreator {
 
 		try{
 			final FileWriter fileWriter = new FileWriter(scriptFile ,false);
-			fileWriter.write("> " + TEST_RESULTS_FILENAME);
-			fileWriter.write('\n');
-			fileWriter.write('\n');
+            fileWriter.write("cd " + OUTPUT_DIR); fileWriter.write('\n');
+            fileWriter.write("command -v bc >/dev/null 2>&1 || { echo >&2 \"I require command bc but it's not installed.  Aborting.\"; exit 1; }"); fileWriter.write('\n');
+            fileWriter.write("command -v time >/dev/null 2>&1 || { echo >&2 \"I require command time but it's not installed.  Aborting.\"; exit 1; }"); fileWriter.write('\n');
+            fileWriter.write("command -v tail >/dev/null 2>&1 || { echo >&2 \"I require command tail but it's not installed.  Aborting.\"; exit 1; }"); fileWriter.write('\n');
+            fileWriter.write("command -v grep >/dev/null 2>&1 || { echo >&2 \"I require command grep but it's not installed.  Aborting.\"; exit 1; }"); fileWriter.write('\n');
+            fileWriter.write("command -v sed >/dev/null 2>&1 || { echo >&2 \"I require command sed but it's not installed.  Aborting.\"; exit 1; }"); fileWriter.write('\n');
+			fileWriter.write("> " + TEST_RESULTS_FILENAME); fileWriter.write('\n');
+            fileWriter.write("echo \"Test results will be written to: " + TEST_RESULTS_FILENAME + "\""); fileWriter.write('\n');
+            fileWriter.write('\n');
 			fileWriter.flush();
 			fileWriter.close();
 
@@ -37,13 +43,10 @@ public class LinuxTestScriptCreator {
 
 	public void writeCommandTest(final String testName, final String linuxEquivalentCommand){
 		try{
-			System.out.println(format("Writing equivalent linux test to: %s", TEST_SCRIPT_FILENAME));
-			System.out.println();
+			System.out.println(format("Writing equivalent linux test to: %s.  Run this script from a shell to produce a native linux/mac/windows-cygwin baseline.", TEST_SCRIPT_FILENAME));
 			final FileWriter fileWriter = new FileWriter(scriptFile ,true);
 			final BufferedWriter scriptFile = new BufferedWriter(fileWriter);
-			final String scriptLine = "startTime=\"$(date +%s%N)\"; " + linuxEquivalentCommand + " > /dev/null; duration=\"$(($(date +%s%N)-startTime))\"; durationInMillis=\"$((duration/1000000))\"; echo \"" + testName + ".executionTime=$durationInMillis\" >> " + TEST_RESULTS_FILENAME;
-			System.out.println(scriptLine);
-			System.out.println();
+            final String scriptLine = "durationInMillis=`(time " + linuxEquivalentCommand + ") 2>&1 | tail -3 | grep real | sed 's/real.*\\([0-9]\\{1,\\}\\)m\\([0-9\\.]\\{1,\\}\\)s/((\\1*60)+\\2)*1000/' | bc | sed 's/\\.000//'` && echo \"" + testName + ".executionTime=$durationInMillis\" >> " + TEST_RESULTS_FILENAME;
 			scriptFile.write(format("echo 'Testing command: %s'", testName));
 			scriptFile.write('\n');
 			scriptFile.write(scriptLine);
@@ -51,7 +54,6 @@ public class LinuxTestScriptCreator {
 			scriptFile.write('\n');
 			scriptFile.flush();
 			scriptFile.close();
-
 		} catch(Exception e){
 			throw new RuntimeException(e);
 		}
