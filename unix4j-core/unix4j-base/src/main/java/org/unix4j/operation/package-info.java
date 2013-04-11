d5f2999864e9cd47487cbfb58b7cc1fd4d6eac31
terz <terzerm@gmail.com>
2013-04-11 18:56:42 -0500

    renamed LineOperationCommand to AdHocCommand

--- unix4j-core/unix4j-base/src/main/java/org/unix4j/operation/package-info.java
+++ unix4j-core/unix4j-base/src/main/java/org/unix4j/operation/package-info.java
@@ -1,7 +1,6 @@
 /**
  * Contains the {@link org.unix4j.operation.LineOperation LineOperation} 
- * interface and the 
- * {@link org.unix4j.operation.LineOperationCommand LineOperationCommand}
- * facilitating the creation of ad-hoc commands.  
+ * interface and the {@link org.unix4j.operation.AdHocCommand AdHocCommand}
+ * implementation facilitating the creation of ad-hoc commands.  
  */
 package org.unix4j.operation;
