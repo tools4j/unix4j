6a98616a1858aa250c5ca260305063b0670812f5
terz <terz@suissoft.com>
2013-07-01 01:28:52 +0200

    faster string comparison for ignore-case substring mode

--- unix4j-core/unix4j-base/src/main/java/org/unix4j/util/StringUtil.java
+++ unix4j-core/unix4j-base/src/main/java/org/unix4j/util/StringUtil.java
@@ -44,7 +44,8 @@ public class StringUtil {
 	 * @return the value as a fixed size string, padded or truncated if
 	 *         necessary
 	 */
-	public static final String fixSizeString(int size, boolean alignLeft, char filler, long value) {
+	public static final String fixSizeString(int size, boolean alignLeft,
+			char filler, long value) {
 		return fixSizeString(size, alignLeft, filler, String.valueOf(value));
 	}
 
@@ -77,7 +78,8 @@ public class StringUtil {
 	 * @return the string {@code s} as a fixed size string, padded or truncated
 	 *         if necessary
 	 */
-	public static final String fixSizeString(int size, boolean alignLeft, String s) {
+	public static final String fixSizeString(int size, boolean alignLeft,
+			String s) {
 		return fixSizeString(size, alignLeft, ' ', s);
 	}
 
@@ -114,7 +116,8 @@ public class StringUtil {
 	 * @return the string {@code s} as a fixed size string, padded or truncated
 	 *         if necessary
 	 */
-	public static final String fixSizeString(int size, boolean alignLeft, char filler, String s) {
+	public static final String fixSizeString(int size, boolean alignLeft,
+			char filler, String s) {
 		if (s.length() < size) {
 			final StringBuilder sb = new StringBuilder(size);
 			if (alignLeft)
@@ -126,7 +129,8 @@ public class StringUtil {
 				sb.append(s);
 			return sb.toString();
 		} else {
-			return alignLeft ? s.substring(0, size) : s.substring(s.length() - size, s.length());
+			return alignLeft ? s.substring(0, size) : s.substring(s.length()
+					- size, s.length());
 		}
 	}
 
@@ -165,7 +169,8 @@ public class StringUtil {
 						index++;
 					}
 				}
-				final Line line = new SingleCharSequenceLine(s, start, lineEndingStart - start, index - lineEndingStart);
+				final Line line = new SingleCharSequenceLine(s, start,
+						lineEndingStart - start, index - lineEndingStart);
 				lines.add(line);
 				start = index;
 			} else {
@@ -173,7 +178,8 @@ public class StringUtil {
 			}
 		}
 		if (start < s.length()) {
-			final Line line = new SingleCharSequenceLine(s, start, s.length() - start, 0);
+			final Line line = new SingleCharSequenceLine(s, start, s.length()
+					- start, 0);
 			lines.add(line);
 		}
 		return lines;
@@ -250,9 +256,9 @@ public class StringUtil {
 
 	/**
 	 * Finds and returns the start of the given sequence after trimming newline
-	 * characters from the left, starting at the given {@code start}
-	 * index. . The following character sequences are treated
-	 * as newline characters: "\n", "\r\n".
+	 * characters from the left, starting at the given {@code start} index. .
+	 * The following character sequences are treated as newline characters:
+	 * "\n", "\r\n".
 	 * 
 	 * @param s
 	 *            the character sequence
@@ -264,14 +270,14 @@ public class StringUtil {
 	 */
 	public static int findStartTrimNewlineChars(CharSequence s, int start) {
 		final int len = s.length();
-		for (int i = start; i < len; ) {
+		for (int i = start; i < len;) {
 			final int ch = s.charAt(i);
 			i++;
 			if (ch != '\n') {
 				if (ch != '\r' || i >= len || s.charAt(i) != '\n') {
-					return i-1;
+					return i - 1;
 				}
-				i++;//increment again, it was "\r\n"
+				i++;// increment again, it was "\r\n"
 			}
 		}
 		return len;
@@ -334,6 +340,139 @@ public class StringUtil {
 			}
 		}
 		return len;
+	}
+
+	/**
+	 * Returns true if and only if the string {@code s} contains the specified
+	 * target string performing case insensitive string comparison.
+	 * 
+	 * @param s
+	 *            the sequence to search for
+	 * @return true if this string contains <code>s</code>, false otherwise
+	 * @throws NullPointerException
+	 *             if <code>s</code> is <code>null</code>
+	 */
+	public static boolean containsIgnoreCase(String source, String target) {
+		return 0 <= indexOfIgnoreCase(source, target);
+	}
+
+	/**
+	 * Tests if this string {@code s} starts with the specified prefix
+	 * performing case insensitive string comparison.
+	 * 
+	 * @param s
+	 *            the string to search
+	 * @param prefix
+	 *            the prefix.
+	 * @return <code>true</code> if the character sequence represented by the
+	 *         argument is a prefix of the character sequence represented by the
+	 *         string s; <code>false</code> otherwise. Note also that
+	 *         <code>true</code> will be returned if the argument is an empty
+	 *         string or is equal to this <code>String</code> object as
+	 *         determined by the {@link #equals(Object)} method.
+	 */
+	public static boolean startsWithIgnoreCase(String s, String prefix) {
+		return 0 == indexOfIgnoreCase(s, prefix, 0);
+	}
+
+	/**
+	 * Returns the index within the source string of the first occurrence of the
+	 * specified target substring performing case insensitive string comparison.
+	 * 
+	 * 
+	 * <p>
+	 * The returned index is the smallest value <i>k</i> for which: <blockquote>
+	 * 
+	 * <pre>
+	 * startsWithIgnoreCase(source.substring(<i>k</i>), target)
+	 * </pre>
+	 * 
+	 * </blockquote> If no such value of <i>k</i> exists, then {@code -1} is
+	 * returned.
+	 * 
+	 * <p>
+	 * Copied from {@code String.indexOf(..)} modified to do case-insensitive
+	 * search. The source is the character array being searched, and the target
+	 * is the string being searched for.
+	 * 
+	 * @param str
+	 *            the substring to search for.
+	 * @return the index of the first occurrence of the specified substring
+	 *         (ignoring the case), or {@code -1} if there is no such
+	 *         occurrence.
+	 */
+	public static final int indexOfIgnoreCase(String source, String target) {
+		return indexOfIgnoreCase(source, target, Integer.MAX_VALUE);
+	}
+
+	/**
+	 * 
+	 * @param source
+	 *            the characters being searched.
+	 * @param target
+	 *            the characters being searched for.
+	 * @param maxIndex
+	 *            the maximum index to return (for instance 0 if only the start
+	 *            of the string is of interest)
+	 */
+	private static int indexOfIgnoreCase(String source, String target,
+			int maxIndex) {
+		if (maxIndex < 0) {
+			throw new IllegalArgumentException("maxIndex cannot be negative: "
+					+ maxIndex);
+		}
+		final int sourceCount = source.length();
+		final int targetCount = target.length();
+		final char first = target.charAt(0);
+		int max = Math.min(maxIndex, sourceCount - targetCount);
+
+		for (int i = 0; i <= max; i++) {
+			final char ch = source.charAt(i);
+			/* Look for first character. */
+			if (!equalsIgnoreCase(ch, first)) {
+				while (++i <= max && !equalsIgnoreCase(ch, first))
+					;
+			}
+
+			/* Found first character, now look at the rest of v2 */
+			if (i <= max) {
+				int j = i + 1;
+				int end = j + targetCount - 1;
+				for (int k = 1; j < end && equalsIgnoreCase(source.charAt(j), target.charAt(k)); j++, k++)
+					;
+
+				if (j == end) {
+					/* Found whole string. */
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * Returns true if the two characters are equal if case is ignored.
+	 * 
+	 * @param ch1
+	 *            the first character
+	 * @param ch2
+	 *            the second character
+	 * @return true if both characters are the same according to
+	 *         case-insensitive comparison
+	 */
+	public static boolean equalsIgnoreCase(char ch1, char ch2) {
+		if (ch1 == ch2)
+			return true;
+		// If characters try converting both characters to uppercase
+		ch1 = Character.toUpperCase(ch1);
+		ch2 = Character.toUpperCase(ch2);
+		if (ch1 == ch2)
+			return true;
+		// Unfortunately, conversion to uppercase does not work properly
+		// for the Georgian alphabet, which has strange rules about case
+		// conversion. So we need to make one last check before
+		// exiting.
+		return Character.toLowerCase(ch1) == Character.toLowerCase(ch2);
 	}
 
 	// no instances
