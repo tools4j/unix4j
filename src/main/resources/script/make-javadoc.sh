2e28790d62a28d726cdb309a73f5eef5bdb36154
Marco Terzer <terzerm@gmail.com>
2018-04-18 22:46:57 +1000

    Update javadoc scripts and remove other obsolte scripts

--- src/main/resources/script/make-javadoc.sh
+++
@@ -1,6 +0,0 @@
-mv pom.xml pom.xml.orig
-sed s/-SNAPSHOT/--SNAPSHOT/g pom.xml.orig > pom.xml
-mvn clean install javadoc:javadoc
-find target/site/apidocs -type f -exec sed -i -e s/--SNAPSHOT/-SNAPSHOT/g {} \;
-rm pom.xml
-mv pom.xml.orig pom.xml
